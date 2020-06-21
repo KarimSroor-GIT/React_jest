6f71248bf2eee54192989a1b7abcd582af8ea90a
karim <karim@Azure.com>
2020-06-21 16:51:39 +1000

    change commentList to a class Component and connecting it to redux store

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
-export default ()=>{
-  return <div> I am Component List</div>
+
+class commentList extends Component {
+
+  renderComments()
+  {
+    return this.props.comments.map( 
+      comment =>{
+        return <li key={comment}>{comment}</li>
+      });
+  };
+
+    render(){
+      return(
+        <div>
+        <ul>
+        {this.renderComments()}
+        </ul>
+        </div>
+      );
+    };
 };
+function mapStateToProps(state){
+return {comments:state.comments}
+}
+export default connect(mapStateToProps,null)(commentList);
